7bebc0a41244f8153a77c3f41ea76e9da6aa2ad1
Sean Choi <yo2seol@gmail.com>
2018-12-13 14:07:07 -0800

    Fixing some build scripts

--- gateway/build.sh
+++ gateway/build.sh
@@ -28,6 +28,15 @@ GIT_COMMIT_MESSAGE=$(git log -1 --pretty=%B 2>&1 | head -n 1)
 GIT_COMMIT_SHA=$(git rev-list -1 HEAD)
 VERSION=$(git describe --all --exact-match `git rev-parse HEAD` | grep tags | sed 's/tags\///' || echo dev)
 
+while true; do
+    read -p "This will delete all docker images in your system. Continue? " yn
+    case $yn in
+        [Yy]* ) docker rmi $(docker images -a -q) || true; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes[Y] or no[N].";;
+    esac
+done
+
 dep ensure
 
 docker build --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy \
