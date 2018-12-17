ca93ae10c627a13729667d57f4312a850ad9155c
Sean Choi <yo2seol@gmail.com>
2018-12-17 15:37:00 -0800

    Removing debug logs

--- gateway/handlers/proxy_helper.go
+++ gateway/handlers/proxy_helper.go
@@ -4,7 +4,6 @@ package handlers
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -16,7 +15,7 @@ import (
 func sendReceiveLambdaNic(addrStr string, port int, data string) string {
 	remoteUDPAddr := net.UDPAddr{IP: net.ParseIP(addrStr), Port: port}
 
-	log.Printf("Connecting to server:%s \n", remoteUDPAddr.String())
+	//log.Printf("Connecting to server:%s \n", remoteUDPAddr.String())
 	conn, err := net.DialUDP("udp4", nil, &remoteUDPAddr)
 	if err != nil {
 		log.Printf("Error: UDP conn error: %v\n", err)
@@ -25,21 +24,21 @@ func sendReceiveLambdaNic(addrStr string, port int, data string) string {
 	defer conn.Close()
 
 	// send to socket
-	log.Printf("Sending to server:%s \n", remoteUDPAddr.String())
-	n, err := conn.Write([]byte(data))
+	//log.Printf("Sending to server:%s \n", remoteUDPAddr.String())
+	_, err = conn.Write([]byte(data))
 	if err != nil {
 		log.Printf("Error in sending to server\n")
 		return ""
 	}
-	log.Printf("Sent %d bytes to server:%s\n", n, remoteUDPAddr.String())
+	//log.Printf("Sent %d bytes to server:%s\n", n, remoteUDPAddr.String())
 	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
-	msg := make([]byte, 512)
-	n, err = conn.Read(msg)
+	msg := make([]byte, 32)
+	n, err := conn.Read(msg)
 	if err != nil {
 		log.Printf("Error in receiving from server\n")
 		return ""
 	}
-	fmt.Printf("Message from server: %d bytes: %s\n", n, string(msg[:n]))
+	//fmt.Printf("Message from server: %d bytes: %s\n", n, string(msg[:n]))
 	return string(msg[:n])
 }
 
