98db8713500c249b624a337dfd9a5ac8150dcb93
Sean Choi <yo2seol@gmail.com>
2018-12-17 15:31:11 -0800

    Debugging proxy again

--- gateway/handlers/proxy_helper.go
+++ gateway/handlers/proxy_helper.go
@@ -10,27 +10,37 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"time"
 )
 
 func sendReceiveLambdaNic(addrStr string, port int, data string) string {
-	remoteUDPAddr := fmt.Sprintf("%s:%d", addrStr, port)
+	remoteUDPAddr := net.UDPAddr{IP: net.ParseIP(addrStr), Port: port}
 
-	log.Printf("Connecting to server:%s \n", remoteUDPAddr)
-	conn, err := net.Dial("udp4", remoteUDPAddr)
+	log.Printf("Connecting to server:%s \n", remoteUDPAddr.String())
+	conn, err := net.DialUDP("udp4", nil, &remoteUDPAddr)
 	if err != nil {
 		log.Printf("Error: UDP conn error: %v\n", err)
 		return ""
 	}
 	defer conn.Close()
+
 	// send to socket
-	log.Printf("Sending to server:%s \n", remoteUDPAddr)
-	fmt.Fprintf(conn, data)
-	log.Printf("Sent to server:%s \n", remoteUDPAddr)
-	var buf bytes.Buffer
-	io.Copy(&buf, conn)
-	fmt.Println("total size:", buf.Len())
-	fmt.Print("Message from server: " + buf.String())
-	return buf.String()
+	log.Printf("Sending to server:%s \n", remoteUDPAddr.String())
+	n, err := conn.Write([]byte(data))
+	if err != nil {
+		log.Printf("Error in sending to server\n")
+		return ""
+	}
+	log.Printf("Sent %d bytes to server:%s\n", n, remoteUDPAddr.String())
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+	msg := make([]byte, 512)
+	n, err = conn.Read(msg)
+	if err != nil {
+		log.Printf("Error in receiving from server\n")
+		return ""
+	}
+	fmt.Printf("Message from server: %d bytes: %s\n", n, string(msg[:n]))
+	return string(msg[:n])
 }
 
 func generateResponse(w http.ResponseWriter, r *http.Request,
